d859a86d2775eeae936a78b01b530c935031b7df
Xavier Sans <xavi.sans.b@gmail.com>
2022-11-21 21:46:43 +0100

    Add error middleware in app

--- src/server/app.ts
+++ src/server/app.ts
@@ -2,6 +2,7 @@ import cors from "cors";
 import express from "express";
 import morgan from "morgan";
 import corsOptions from "./cors/corsOptions.js";
+import { generalError, notFoundError } from "./middlewares/errors/errors.js";
 
 const app = express();
 
@@ -13,5 +14,8 @@ app.disable("x-powered-by");
 app.use((req, res) => {
   res.status(200).json({ message: "Build in progress" });
 });
+
+app.use(notFoundError);
+app.use(generalError);
 
 export default app;
