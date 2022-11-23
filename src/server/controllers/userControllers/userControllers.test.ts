60311ccd978875fb84a0dc0ce491d7599a4fae70
Xavier Sans <xavi.sans.b@gmail.com>
2022-11-23 16:23:28 +0100

    Add login test

--- src/server/controllers/userControllers/userControllers.test.ts
+++ src/server/controllers/userControllers/userControllers.test.ts
@@ -1,7 +1,11 @@
 import type { NextFunction, Request, Response } from "express";
-import type { Credentials } from "./types";
+import bcrypt from "bcrypt";
+import type { Credentials, RegisterData } from "./types";
 import User from "../../../database/models/User.js";
-import { registerUser } from "./userControllers";
+import { loginUser, registerUser } from "./userControllers";
+import errorsMessageSet from "../../../CustomError/errorsMessageSet";
+
+const { wrongCredentialsPassword, wrongCredentialsUsername } = errorsMessageSet;
 
 beforeEach(() => {
   jest.clearAllMocks();
@@ -15,12 +19,13 @@ const res: Partial<Response> = {
 const next = jest.fn();
 
 describe("Given a register controller", () => {
-  const registerBody: Credentials = {
+  const registerBody: RegisterData = {
     username: "xavi",
     password: "12345",
+    email: "xav@i.com",
   };
 
-  describe("When it receives a username 'xavi' and a password '12345' and the creation gets rejected", () => {
+  describe("When it receives a username 'xavi', email 'xav@i.com' and a password '12345' and the creation gets rejected", () => {
     test("Then it should invoke its method next", async () => {
       const req: Partial<Request> = {
         body: registerBody,
@@ -34,7 +39,7 @@ describe("Given a register controller", () => {
     });
   });
 
-  describe("When it receives a username 'xavi' and a password '12345'", () => {
+  describe("When it receives a username 'xavi', email 'xav@i.com' and a password '12345'", () => {
     test("Then it should invoke its method status with 201 and its method json with the received user id and the username", async () => {
       const expectedStatus = 201;
 
@@ -48,6 +53,54 @@ describe("Given a register controller", () => {
 
       expect(res.status).toHaveBeenCalledWith(expectedStatus);
       expect(res.json).toHaveBeenCalled();
+    });
+  });
+});
+
+describe("Given a login controller", () => {
+  const loginBody: Credentials = {
+    username: "xavi",
+    password: "12345",
+  };
+
+  const req: Partial<Request> = {
+    body: loginBody,
+  };
+
+  describe("When When it receives a  correct username 'xavi' and a correct password '12345'", () => {
+    test("Then it should invoke the response method with a 200 status and its json method with a valid token", async () => {
+      const expectedStatus = 200;
+      User.findOne = jest.fn().mockResolvedValueOnce(loginBody);
+      bcrypt.compare = jest.fn().mockResolvedValueOnce(true);
+
+      await loginUser(req as Request, res as Response, next as NextFunction);
+
+      expect(res.status).toHaveBeenCalledWith(expectedStatus);
+      expect(res.json).toHaveBeenCalled();
+    });
+  });
+
+  describe("When it receives a request with an invalid username", () => {
+    test("Then it should invoke the next function with a username error", async () => {
+      User.findOne = jest.fn().mockResolvedValueOnce(null);
+      const usernameError = wrongCredentialsUsername;
+
+      await loginUser(req as Request, res as Response, next as NextFunction);
+
+      expect(next).toBeCalledWith(usernameError);
+    });
+  });
+
+  describe("When it receives a valid username 'xavi' and the wrong password", () => {
+    test("Then it should invoke the next function with a password error", async () => {
+      User.findOne = jest.fn().mockResolvedValueOnce(loginBody);
+      const passwordError = wrongCredentialsPassword;
+
+      bcrypt.compare = jest.fn().mockResolvedValueOnce(false);
+
+      await loginUser(req as Request, res as Response, next as NextFunction);
+
+      expect(next).toBeCalledWith(passwordError);
     });
   });
 });
