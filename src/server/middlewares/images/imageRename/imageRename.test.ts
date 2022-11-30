0ce36d3709987a46ca8efc513048c28efedc069d
Xavier Sans <xavi.sans.b@gmail.com>
2022-11-30 12:41:04 +0100

    First draft for rename test

---
+++ src/server/middlewares/images/imageRename/imageRename.test.ts
@@ -0,0 +1,9 @@
+const timestamp = Date.now();
+jest.useFakeTimers();
+jest.setSystemTime(timestamp);
+
+describe("Given", () => {
+  describe("When", () => {
+    test("Then", () => {});
+  });
+});
